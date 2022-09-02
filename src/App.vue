8e25d59c47ee14c45ee958ec589339c3176606f7
cebbz <sebastien.capron@gmail.com>
2022-09-02 18:54:05 +0200

    Setting up the header and footer

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,9 @@ import Cart from './components/Cart/Cart.vue';
 
 <template>
     <div class="app-container">
-        <TheHeader class="header b1" />
-        <Shop class="shop b2" />
-        <Cart class="cart b3" />
+        <TheHeader class="header" />
+        <!-- <Shop class="shop b2" />
+        <Cart class="cart b3" />-->
         <TheFooter class="footer b4" />
     </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,25 @@
 <template>
-    <header>
-        <h1>Hearder</h1>
+    <header class="px-20 d-flex flex-row align-items-center">
+        <a href="#" class="d-flex flex-row align-items-center mr-10">
+            <img src="../assets/logo.svg" alt="logo" />
+            <span class="logo">Cebbz</span>
+        </a>
+        <ul class="d-flex flex-row flex-fill">
+            <li class="mr-20">
+                <a href="#">Boutique</a>
+            </li>
+            <li>
+                <a href="#">Admin</a>
+            </li>
+        </ul>
+        <ul class="d-flex flex-row">
+            <li class="mr-10">
+                <a href="#">Connexion</a>
+            </li>
+            <li>
+                <a href="#">Inscription</a>
+            </li>
+        </ul>
     </header>
 </template>
 
@@ -8,4 +27,21 @@
 </script>
 
 <style lang="scss" scoped>
+header {
+    background-color: var(--primary-1);
+
+    a {
+        color: var(--text-primary-color);
+
+        img {
+            width: 20px;
+            margin-right: 5px;
+        }
+
+        .logo {
+            font-weight: 700;
+            font-size: 20px;
+        }
+    }
+}
 </style>
