66c62fdfa8913037b82b2334ab9f941413af237a
cebbz <sebastien.capron@gmail.com>
2022-09-02 15:08:49 +0200

    Implementation of debugging utilities class

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
 </script>
 
 <template>
-    <h1>Hello world !</h1>
+    <h1 class="b1">Hello world !</h1>
 </template>
 
 <style lang="scss">
 @import "./assets/base.scss";
+@import "./assets/debug.scss";
 </style>
