ce0de27bef87f8360adacb5f37bd783967698469
cebbz <sebastien.capron@gmail.com>
2022-09-04 16:17:02 +0200

    Display of total

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import TheFooter from './components/Footer.vue';
 import Shop from './components/Shop/Shop.vue';
 import Cart from './components/Cart/Cart.vue';
 import data from './data/product';
-import { reactive } from 'vue';
+import { computed, reactive } from 'vue';
 import type { ProductCartInterface, ProductInterface } from './interfaces';
 
 const state = reactive<{
@@ -41,20 +41,24 @@ function removeProductFromCart(productId: number): void {
     }
 }
 
+const cartEmpty = computed(() => state.cart.length === 0);
+
 </script>
 
 <template>
-    <div class="app-container">
+    <div class="app-container" :class="{
+        gridEmpty: cartEmpty,
+    }">
         <TheHeader class="header" />
         <Shop :products="state.products" @add-product-to-cart="addProductToCart" class="shop" />
-        <Cart :cart="state.cart" class="cart" @remove-product-from-cart="removeProductFromCart" />
+        <Cart v-if="!cartEmpty" :cart="state.cart" class="cart" @remove-product-from-cart="removeProductFromCart" />
         <TheFooter class="footer" />
     </div>
 </template>
 
 <style lang="scss">
-@import "./assets/base.scss";
-@import "./assets/debug.scss";
+@import "./assets/scss/base.scss";
+@import "./assets/scss/debug.scss";
 
 .app-container {
     min-height: 100vh;
@@ -62,6 +66,11 @@ function removeProductFromCart(productId: number): void {
     grid-template-areas: "header header""shop cart""footer footer";
     grid-template-columns: 75% 25%;
     grid-template-rows: 48px auto 48px;
+}
+
+.gridEmpty {
+    grid-template-areas: "header""shop""footer";
+    grid-template-columns: 100%;
 }
 
 .header {
--- src/components/Cart/Cart.vue
+++ src/components/Cart/Cart.vue
@@ -1,20 +1,29 @@
 <template>
-    <div class="p-20">
+    <div class="p-20 d-flex flex-column">
         <h2 class="mb-10">Panier</h2>
-        <CartProductList :cart="cart" @remove-product-from-cart="emit('remove-product-from-cart', $event)" />
+        <CartProductList class="flex-fill" :cart="cart"
+            @remove-product-from-cart="emit('remove-product-from-cart', $event)" />
+        <button class="btn btn-success">Commander ({{ totalPrice }}€)</button>
     </div>
 </template>
 
 <script setup lang="ts">
 import CartProductList from './CartProductList.vue';
 import type { ProductCartInterface } from '@/interfaces';
-defineProps<{
+import { computed } from 'vue';
+const props = defineProps<{
     cart: ProductCartInterface[];
 }>();
 
 const emit = defineEmits<{
         (e: 'remove-product-from-cart', productId: number): void;
     }>();
+
+const totalPrice = computed(() => {
+    return props.cart.reduce((total, product) => {
+        return total + product.price * product.quantity;
+    }, 0);
+});
 </script>
 
 <style lang="scss" scoped>
